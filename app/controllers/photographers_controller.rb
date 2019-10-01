e123c447a2a2533d5890196e37c0a553a9f4a911
Mark Del Mar <mark@1000martians.com>
2019-10-01 14:00:56 -0400

    completed seed file

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -1,2 +1,11 @@
 class PhotographersController < ApplicationController
+
+  def index
+    @photographers = Photographer.all
+  end
+
+  def show
+    @photographer = Photographer.find(params[:id])
+  end
+
 end
